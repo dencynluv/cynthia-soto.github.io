7a4652b5606aeea2a19f44c730e5051c75c03bf4
Cynthia Soto <sotocyn@gmail.com>
2016-08-30 13:20:29 -0400

    Completed the Skills section with animated progress bars. Added javascript file - script.js that contains code for animated progress bars.

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+  $('.html').css('width', '50%');
+  $('.css').css('width', '50%');
+  $('.jquery').css('width', '50%');
+  $('.javascript').css('width', '50%');
+});
