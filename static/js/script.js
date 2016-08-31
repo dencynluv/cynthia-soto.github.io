d5de3bd98f5c798cefb6fba61a4d09b4b47935c5
Cynthia Soto <sotocyn@gmail.com>
2016-08-31 01:45:19 -0400

    Added favicon image. Added scrollspy to navbar, now navbar links become 'active' when scrolling through each section. It lets you know what section on the page you are by highliting the link. Fixed animation on progress bars in Skills section to animate when scrolling down the page or clicking the link on the navbar or when using the arrow down to go down the page. Portfolio is now completed.

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,32 @@
+//  SKILLS PROGRESS BAR ANIMATION
 
-$(document).ready(function() {
+$('#skills').on('mouseover', function() {
   $('.front-end').css('width', '70%');
   $('.back-end').css('width', '60%');
   $('.cs-fund').css('width', '50%');
   $('.tools').css('width', '80%');
 });
+
+$('#skills-link').on('click', function() {
+  $('.front-end').css('width', '70%');
+  $('.back-end').css('width', '60%');
+  $('.cs-fund').css('width', '50%');
+  $('.tools').css('width', '80%');
+});
+
+$(window).on('keydown', function() {
+  $('.front-end').css('width', '70%');
+  $('.back-end').css('width', '60%');
+  $('.cs-fund').css('width', '50%');
+  $('.tools').css('width', '80%');
+});
+
+
+// SCROLLSPY 
+
+// $(document).ready(function() {
+//     $("body").scrollspy({
+//         target: "#myNavbar",
+//         offset: 70
+//     })
+// });
