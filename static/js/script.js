b07d2d5e74d3be19ce1a3a9890f3f1b026c05ee6
Cynthia Soto <sotocyn@gmail.com>
2016-08-31 23:28:24 -0400

    Fixed the skills section progress bars. They are now responsive as is the rest of the site. Cleaned up some more css. Updated the progress bars percentages on the js file. Site is now live

--- static/js/script.js
+++ static/js/script.js
@@ -2,21 +2,21 @@
 
 $('#skills').on('mouseover', function() {
   $('.front-end').css('width', '70%');
-  $('.back-end').css('width', '60%');
+  $('.back-end').css('width', '55%');
   $('.cs-fund').css('width', '50%');
   $('.tools').css('width', '80%');
 });
 
 $('#skills-link').on('click', function() {
   $('.front-end').css('width', '70%');
-  $('.back-end').css('width', '60%');
+  $('.back-end').css('width', '55%');
   $('.cs-fund').css('width', '50%');
   $('.tools').css('width', '80%');
 });
 
 $(window).on('keydown', function() {
   $('.front-end').css('width', '70%');
-  $('.back-end').css('width', '60%');
+  $('.back-end').css('width', '55%');
   $('.cs-fund').css('width', '50%');
   $('.tools').css('width', '80%');
 });
